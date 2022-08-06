ab2cd32484364eb2e319f9fd43747b2c9865918a
Mahir Rahman <rahmanm7@miamioh.edu>
2022-08-06 16:09:09 -0400

    Added ship movement capabilties like moving left and right, speed adjustment, and prevention of ship going off screen

--- ship.py
+++ ship.py
@@ -5,11 +5,28 @@ class Ship:
     def __init__(self, ai_game):
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
         # Loads the ship image and gets its rectangle
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect() 
         self.rect.midbottom = self.screen_rect.midbottom
+
+        self.x = float(self.rect.x)
+
+        # Movement flags
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        '''Ship position is updated based on the movement flag'''
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # This allows us to record the changes in self.x to our rectangle
+        self.rect.x = self.x
 
     def blitme(self):
         '''Draws the ship in its current position'''
